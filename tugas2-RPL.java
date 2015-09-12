6cf8b9a72a10606225e710da09beef65db72ee10
rizqyarrijalm <sahabatridwan2@gmail.com>
2015-09-12 22:43:53 +0700

    percobaan

---
+++ tugas2-RPL.java
@@ -0,0 +1,32 @@
+//tugas rpl 2
+1).Alasan perlunya Rekayasa Perangkat Lunak :
+	1. Perkembangan perangkat keras yang lebih cepat
+	2. Kemampuan untuk membangun yang dituntut
+		untuk memenuhi kebutuhan secara cepat
+	3. Peningkatan ketergantungan pada perangkat lunak
+	4. Pembangunan perangkat lunak berkualitas tinggi dan dapat dipercaya
+	5. Desain yang kurang dan minimnya sumber daya
+	
+	tujuan yang lain dari Rekayasa Perangkat Lunak (RPL), yaitu :
+	1.  Meningkatkan keakuratan, performance & efficiency produk secara                      keseluruhan dalam pengembangan.
+	2.  Menerapkan metodologi yang terdefinisi dengan baik untuk resolusi software.
+	3.  Melengkapi secara rasional konflik-konflik dan dokumentasi
+//sumber : https://rplfajar.wordpress.com/2012/03/10/hello-world/
+//http://lengarifika.blogspot.com/2012/01/rekayasa-perangkat-lunak-rpl.html
+
+2)jelaskan perbedaan PL dengan RPL !
+	 Perangkat lunak adalah seluruh perintah yang digunakan untuk memproses informasi.
+	 Perangkat lunak dapat berupa program atau prosedur.
+			Program adalah kumpulan perintah yang dimengerti oleh komputer sedangkan
+			prosedur adalah perintah yang dibutuhkan oleh pengguna dalam memproses informasi. (O’Brien, 1999).
+	RPL sendiri adalah Suatu disiplin ilmu yang membahas semua aspek produksi perangkat lunak, 
+			mulai dari tahap awal yaitu analisa kebutuhan pengguna, 
+			menentukan spesifikasi dari kebutuhan pengguna, disain, pengkodean, pengujian sampai pemeliharaan sistem setelah digunakan.
+	RPL tidak hanya berhubungan dengan cara pembuatan program komputer. Pernyataan “semua aspek produksi” pada pengertian di atas, 
+		mempunyai arti semua hal yang berhubungan dengan proses produksi seperti manajemen proyek, penentuan personil,
+		anggaran biaya, metode, jadwal, kualitas sampai dengan pelatihan pengguna merupakan bagian dari RPL.	
+//sumber : http://lengarifika.blogspot.com/2012/01/rekayasa-perangkat-lunak-rpl.html
+		
+3) berikan 2 pendapat mengenai PL dan RPL 
+4) berikan perbandingan mengenai model proses RAD dgn RUP
+5) berikan penjelasan model proses yg tepat utk tugas masing2.
